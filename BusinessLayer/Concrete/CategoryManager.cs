f26f460f5712ed3de4c8e69868a359a3b2407e05
nilsu <nilsu@DESKTOP-O6Q5UAT>
2022-10-01 21:44:15 +0300

    UPDATE

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        public object GetAllBL()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
